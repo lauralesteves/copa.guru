7359a31b1613a4e8b3bdd7d08f64d21bf4d0e323
Laura Esteves <git@lauraesteves.com>
2026-04-07 15:23:39 -0300

    feat(models): add match and prediction domain models

---
+++ backend/internal/models/prediction.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type Prediction struct {
+	ID          bson.ObjectID `json:"id" bson:"_id,omitempty"`
+	UserID      bson.ObjectID `json:"userId" bson:"userId"`
+	MatchID     bson.ObjectID `json:"matchId" bson:"matchId"`
+	MatchNumber int           `json:"matchNumber" bson:"matchNumber"`
+	Goals1      int           `json:"goals1" bson:"goals1"`
+	Goals2      int           `json:"goals2" bson:"goals2"`
+	Points      *int          `json:"points" bson:"points,omitempty"`
+	CreatedAt   time.Time     `json:"createdAt" bson:"createdAt"`
+	UpdatedAt   time.Time     `json:"updatedAt" bson:"updatedAt"`
+}
+
+type PointsUpdate struct {
+	PredictionID bson.ObjectID
+	Points       int
+}
+
+type ScoreResult struct {
+	Total          int `json:"total"`
+	ExactScores    int `json:"exactScores"`
+	CorrectResults int `json:"correctResults"`
+}
